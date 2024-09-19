4f150d35f990e397d1cbbba9fbfc4d0ebc0bb55f
Natalie <natapoke2@gmail.com>
2024-09-19 09:57:18 -0400

    modifying hello.py to render html templates

--- pra2/hello.py
+++ pra2/hello.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# EXAMPLE 2-1
 # defines application instance and a single route and view function.
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World!</h1>'
+
+# EXAMPLE 2-2
+# dynamic url in browser, presented with a personalized greeting that includes the name provided in the URL
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello, {}!</h1>'.format(name)
+
+# EXAMPLE 3-3
+# rendering templates
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
-# dynamic url in browser, presented with a personalized greeting that includes the name provided in the URL
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
