85ef93114586b3a6bfe2d438af7c14751cde9fef
Natalie <natapoke2@gmail.com>
2024-09-19 09:48:03 -0400

    adding gitignore, and implementing dynamic route to hello.py Flask app

--- pra2/hello.py
+++ pra2/hello.py
@@ -1,8 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+# defines application instance and a single route and view function.
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
-# defines application instance and a single route and view function.
+# dynamic url in browser, presented with a personalized greeting that includes the name provided in the URL
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
