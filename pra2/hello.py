6890e2d9c0f70cfef684a9a5a9fc3c9ff93c32f8
Natalie <natapoke2@gmail.com>
2024-09-24 01:58:07 -0400

    example 4-4 handling a web form with GET/POST request methods

--- pra2/hello.py
+++ pra2/hello.py
@@ -3,7 +3,21 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+import os
+
 app = Flask(__name__)
+
+# use os to generate a secret key
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+class NameForm (FlaskForm): 
+    name = StringField('What is your name?', validators=[DataRequired()]) 
+    submit = SubmitField('Submit')
 
 # EXAMPLE 3-4 Flask-Bootstrap initialization
 bootstrap = Bootstrap(app)
@@ -22,11 +36,19 @@ moment = Moment(app)
 #     return '<h1>Hello, {}!</h1>'.format(name)
 
 # EXAMPLE 3-3 Rendering template
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # EXAMPLE 3-13 adding a datetime variable
     print(datetime.utcnow())
-    return render_template('index.html', current_time=datetime.utcnow())
+
+    # EXAMPLE 4-4 handle webform with GET and POST request methods
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -40,3 +62,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
