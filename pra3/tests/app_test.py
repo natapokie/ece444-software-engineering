8540454baca9b25ef8be3f687f0869dfc55c1d40
Natalie Chan <natalie.chan@stonex.com>
2024-10-02 00:12:12 -0400

    adding login required to delete post

--- pra3/tests/app_test.py
+++ pra3/tests/app_test.py
@@ -78,7 +78,11 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
 
@@ -100,6 +104,12 @@ def test_search(client):
         query_string={"query": "Hello"},
         follow_redirects=True,
     )
-
     assert b"&lt;Hello&gt;" in rv_search.data
     assert b"<strong>HTML</strong> allowed here" in rv_search.data
+
+    rv_search = client.get(
+        "/search/",
+        query_string={"query": "Bye"},
+        follow_redirects=True,
+    )
+    assert b"" in rv_search.data
